5781aef3ec90e4bbd1636db1ea946fc966a56ba6
Oliquen <olivier.nella@outlook.fr>
2018-03-22 09:55:42 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link rel="shortcut icon" href="#" type="image/x-icon">
+        <title>ARG La Gavotte</title>
+
+        <?php require "includes/head.php" ?>
+
+
+    </head>
+
+    <body>
+
+        <section id="dropdown-menu-o" data-rv-view="22">
+
+            <nav class="navbar navbar-dropdown navbar-fixed-top">
+
+                <div class="container">
+
+                    <div class="navbar-brand">
+
+                        <a class="text-black" href="index.html"><strong>Association des Résidents de la Gavotte</strong></a>
+                    </div>
+
+                    <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse"
+                            data-target="#exCollapsingNavbar">
+                        <div class="hamburger-icon"></div>
+                    </button>
+
+                    <ul class="nav-dropdown collapse pull-xs-right navbar-toggleable-sm nav navbar-nav" id="exCollapsingNavbar">
+                        <li class="nav-item"><a class="nav-link link" href="index.html">Accueil</a></li>
+                        <li class="nav-item dropdown open"><a class="nav-link link dropdown-toggle"
+                                                              data-toggle="dropdown-submenu" href="page1.html"
+                                                              aria-expanded="true">Services</a>
+                            <div class="dropdown-menu"><a class="dropdown-item" href="page8.html">Babysitting</a>
+                                <div class="dropdown open"><a class="dropdown-item dropdown-toggle"
+                                                              data-toggle="dropdown-submenu" href="page9.html"
+                                                              aria-expanded="true">Nos Artisans</a>
+                                    <div class="dropdown-menu dropdown-submenu"><a class="dropdown-item" href="page10.html">Electriciens</a><a
+                                            class="dropdown-item" href="page11.html">Plombiers</a><a class="dropdown-item"
+                                                                                                     href="page12.html">Paysagistes</a><a
+                                            class="dropdown-item" href="page13.html">Aide à Domicile</a><a class="dropdown-item"
+                                                                                                           href="#/">Piscinistes</a><a
+                                            class="dropdown-item" href="https://mobirise.com/"></a><a class="dropdown-item"
+                                                                                                      href="#/"></a><a
+                                            class="dropdown-item" href="https://mobirise.com/"></a></div>
+                                </div>
+                                <a class="dropdown-item" href="https://mobirise.com/"></a><a class="dropdown-item"
+                                                                                             href="#"></a>
+                            </div>
+                        </li>
+                        <li class="nav-item"><a class="nav-link link" href="page2.html" aria-expanded="false">Actualités</a>
+                        </li>
+                        <li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" href="page3.html"
+                                                         aria-expanded="false" data-toggle="dropdown-submenu">Evénements</a>
+                            <div class="dropdown-menu"><a class="dropdown-item" href="#">Repas de Noel</a><a
+                                    class="dropdown-item" href="#">Fête des Voisins</a><a class="dropdown-item" href="#">On fête
+                                    l'été !!</a></div>
+                        </li>
+                        <li class="nav-item"><a class="nav-link link" href="page4.html" aria-expanded="false">A Propos</a></li>
+                        <li class="nav-item"><a class="nav-link link" href="page5.html" aria-expanded="false">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link link" href="https://mobirise.com/" aria-expanded="false"></a>
+                        </li>
+                        <li class="nav-item"><a class="nav-link link" href="https://mobirise.com/" aria-expanded="false"></a>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </nav>
+
+        </section>
+
+
+
+        <section id="video-container">
+
+
+            <video autoplay loop muted poster="images/poster.jpg" >
+                <source src="video/stvictoire.mp4">
+            </video>
+
+            <h1 class="titre">Association des résidents de la Gavotte</h1>
+
+            <ol class="carousel-indicators">
+                <div class="mouse_scroll">
+
+                    <div class="mouse">
+                        <div class="wheel"></div>
+                    </div>
+                    <div>
+                        <span class="m_scroll_arrows unu"></span>
+                        <span class="m_scroll_arrows doi"></span>
+                        <span class="m_scroll_arrows trei"></span>
+                    </div>
+                </div>
+            </ol>
+
+        </section>
+
+        <section class="mbr-section mbr-section-small" id="msg-box1-1z" data-rv-view="15"
+                 style="background-color: rgb(255, 248, 224); padding-top: 0rem; padding-bottom: 1.5rem;">
+
+
+            <div class="container">
+                <div class="row">
+
+                    <div>
+
+                        <div class="col-sm-8">
+
+                            <p class="lead"><br><br>Bienvenue sur le site de l'association des résidents de la Gavotte :)<br><br>Ce site
+                                à vocation à permettre aux habitants de la Gavotte et plus largement à tous les habitants de
+                                Chateauneuf le Rouge le désirant de partager des informations utiles, des bons plans et une
+                                certaine idée du voisinage 2.0. Alors n'hésitez pas à abuser de ce site ;)</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="mbr-section mbr-section-nopadding" id="map1-7" data-rv-view="18">
+            <div class="mbr-map">
+                <iframe frameborder="0" style="border:0"
+                        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0Dx_boXQiwvdz8sJHoYeZNVTdoWONYkU&amp;q=place_id:ChIJg65xx8SayRIRcQTXdhGLLBg"
+                        allowfullscreen=""></iframe>
+            </div>
+        </section>
+
+
+
+
+    </body>
+</html>
